3baa5918ee7242d5bf48d82f3eed0372fe3eca0d
itrecipe <itrecipe@naver.com>
2023-04-17 10:18:38 +0900

    spring_exam
    23.4.17

--- ex00/src/test/java/com/kook/ex00/persistence/TimeMapperTests.java
+++ ex00/src/test/java/com/kook/ex00/persistence/TimeMapperTests.java
@@ -20,9 +20,16 @@ public class TimeMapperTests {
 	private TimeMapper timeMapper; //인터페이스 참조 변수 주입
 	
 	@Test
+	//mybatis @Select 어노테이션 사용
 	public void testGetTime() {
 		log.info(timeMapper.getClass().getName());
 		log.info(timeMapper.getTime()); //getTime추상메서드의 어노테이션 @Select("SELECT sysdate FROM dual")을 실행한다.
 	}
-
+	
+	@Test
+	//mybatis xml사용
+	public void testGetTime2() {
+		log.info("getTime2");
+		log.info(timeMapper.getTime2()); //getTime추상메서드의 어노테이션 @Select("SELECT sysdate FROM dual")을 실행한다.
+	}
 }
