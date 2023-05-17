58cd88ff816354aee29b01cc8b2eab52e904a997
IT Recipe <itrecipe@naver.com>
2023-05-18 00:23:07 +0900

    spring_exam
    235.18

--- ex02/src/main/java/org/ezen/ex02/service/BoardServiceImpl.java
+++ ex02/src/main/java/org/ezen/ex02/service/BoardServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import lombok.AllArgsConstructor;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
