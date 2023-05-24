b079b4c50eac03fb3a0edf79282748c91345149b
itrecipe <itrecipe@naver.com>
2023-05-24 11:27:10 +0900

    spring_exam
    23.5.24

--- ex02/src/main/java/org/ezen/ex02/security/CustomUserDetailsService.java
+++ ex02/src/main/java/org/ezen/ex02/security/CustomUserDetailsService.java
@@ -13,22 +13,24 @@ import lombok.extern.log4j.Log4j;
 
 @Log4j
 public class CustomUserDetailsService implements UserDetailsService {
-
-	@Setter(onMethod_= {@Autowired})
+	
+	@Setter(onMethod_ = { @Autowired })
 	private MemberMapper memberMapper;
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		//여기서 주의사항은 파라미터 username은 form에서 userid를 말하고 security에서는 username이라고 부름
+		//파라메터 username은 form에서는 userid를 말함
 		
 		log.warn("Load User By UserName : " + username);
 		
 		MemberVO vo = memberMapper.read(username);
-		
-		log.warn("queried by member mapper : " + vo);
-		
+
+		log.warn("queried by member mapper: " + vo);
+
 		return vo == null ? null : new CustomUser(vo);
-		//리턴 타입이 인터페이스 UserDetails이므로 이를 구현한 클래스 객체여야 하는데
-		//CustomUser객체는 UserDetails를 구현한 User클래스를 상속한 객체이다.
+		//리턴타입이 인터페이스UserDetails이므로 이를 구현한 클래스 객체여야 하는데
+		//CustomUser객체는 UserDetails를 구현한 User클래스를 상속한 객체이다
+		
 	}
+
 }
